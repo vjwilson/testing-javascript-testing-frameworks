2d549ca1da391b21fb6d466f5a0134cddee032c6
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-08-20 14:32:03 -0400

    Add jest testing, limit ava to /test directory

---
+++ __tests__/Math.spec.js
@@ -0,0 +1,38 @@
+jest.unmock('../Math/Math'); // unmock to use the actual implementation of Math
+
+var math = require('../Math/Math');
+
+describe("Math", function() {
+  var firstOperand;
+  var secondOperand;
+
+  beforeEach(function() {
+    firstOperand = 2;
+    secondOperand = 3;
+  });
+
+  it("should add two numbers", function() {
+    var result = math.add(firstOperand, secondOperand);
+    expect(result).toEqual(firstOperand + secondOperand);
+  });
+
+  it("should subtract two numbers", function() {
+    var result = math.subtract(firstOperand, secondOperand);
+    expect(result).toEqual(firstOperand - secondOperand);
+  });
+
+  it("should multiply two numbers", function() {
+    var result = math.multiply(firstOperand, secondOperand);
+    expect(result).toEqual(firstOperand * secondOperand);
+  });
+
+  it("should divide two numbers", function() {
+    var result = math.divide(firstOperand, secondOperand);
+    expect(result).toEqual(firstOperand / secondOperand);
+  });
+
+  it("should divide return undefined when the divisor is 0", function() {
+    var result = math.divide(firstOperand, 0);
+    expect(result).not.toBeDefined();
+  });
+});
